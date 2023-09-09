81288a20d461fbe50def986981c81b170dbb7a68
aastro_main <cr0ntoz@gmail.com>
2023-09-09 21:38:51 +0200

    tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Error;
+
+
+
+fn change(value: i32) -> Result<i32, Error> {
+    if value < 0 {
+        Err(Error::new(std::io::ErrorKind::InvalidInput, "Negative value provided"))
+    } else {
+        Ok(value)
+    }
+}
+
+#[test]
+fn zero_test(){
+    assert_eq!(change(0).unwrap(), 0);
+}
+
+
+#[test]
+fn twelve_test() {
+    assert_eq!(change(12).unwrap(), 3);
+}
+
+#[test]
+fn four_sixty_eight_test() {
+    assert_eq!(change(468).unwrap(), 11);
+}
+
+#[test]
+fn one_two_three_four_five_six_test() {
+    assert_eq!(change(123456).unwrap(), 254);
 }
